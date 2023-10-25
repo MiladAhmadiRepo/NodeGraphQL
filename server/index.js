cd0aacf96cd3ccf7e77ac0032655384ffadee2fe
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2023-10-25 11:17:22 -0700

    add env

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express =require('express');
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 
 var app = express()
+app.listen(port,console.log(`Serve is running on port  ${port}`));
