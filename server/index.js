f429a4261cc67821ca9edf5dce344ed4cfc8a136
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2023-10-25 11:25:35 -0700

    add schema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express =require('express');
 require('dotenv').config();
+const { graphqlHTTP } = require("express-graphql")
+const schema=require('./schema/schema')
 const port = process.env.PORT || 5000
 
 
 var app = express()
+
+//we use ((process.env.NODE_ENV==='develop')) instead of ((true))
+app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: process.env.NODE_ENV==='develop',
+    })
+  )
 app.listen(port,console.log(`Serve is running on port  ${port}`));
